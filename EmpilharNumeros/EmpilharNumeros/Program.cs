55f39467341b29832ff4fa8387048700609598f2
= <=>
2021-06-18 19:19:26 -0300

    Realização do exercícios 7, empilhando números

---
+++ EmpilharNumeros/EmpilharNumeros/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EmpilharNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            int numero;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Informe um número");
+                numero = int.Parse(Console.ReadLine());
+
+                array[i] = numero;
+            }
+
+            Console.WriteLine("Posição inicial da pilha.");
+
+            foreach (int value in array) Console.WriteLine("[" + value + "]");
+
+
+            Array.Reverse(array);
+
+            Console.WriteLine("Posição invertida da pilha.");
+            foreach (int value in array) Console.WriteLine("[" + value + "]");
+
+
+            Console.ReadKey();
+        }
+    }
+}
